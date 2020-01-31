89349403ba924a2652568b0f7dd76a784b5a0f2c
Administrator <Administrator@RPK-20200131HCX>
2020-01-31 23:35:32 +0800

    999999999

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,14 +2,14 @@
 //
 
 #include "stdafx.h"
-
-
+#include <iostream>
+using namespace std;
 int main()
 {
 
-
+	cout << "dddddddddd" << endl;
 	///////////;;;;;;;;;;;;
-
+	getchar();
 
     return 0;
 }
